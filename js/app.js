fb4af0e8af2c96ecb5d4edbe86eee4d92bf66143
Julia Dlutek <jdlutek987@gmail.com>
2022-05-26 13:13:57 +0200

    Display fetched articles

--- js/app.js
+++ js/app.js
@@ -1,31 +1,47 @@
 const URL = "https://api.spaceflightnewsapi.net/v3/articles/";
+let mainContent;
 
-fetch(URL)
-  .then((res) => res.json())
-  .then((data) => {
-    data.map((article) => {
-      console.log(article);
-    });
-  })
-  .catch((err) => console.error(err));
+const main = () => {
+  prepareDomElements();
+  prepareDomEvents();
+  fetchArticles();
+};
+
+const prepareDomElements = () => {
+  mainContent = document.querySelector(".main-content");
+};
+
+const prepareDomEvents = () => {};
+
+const fetchArticles = () => {
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      data.map((article) => {
+        const newArticle = createArticleCard(article);
+        mainContent.append(newArticle);
+      });
+    })
+    .catch((err) => console.error(err));
+};
 
 const createArticleCard = (article) => {
   const articleCard = document.createElement("article");
   articleCard.classList.add("article");
-  article.append(articleCard);
 
   const articleImage = document.createElement("img");
   articleImage.classList.add("article-image");
-  articleImage.setAttribute("src", article.imageURL);
+  articleImage.setAttribute("src", article.imageUrl);
+  articleImage.setAttribute("alt", "article image");
 
   const articleDetails = document.createElement("div");
-  articleDetails.add("article-details");
+  articleDetails.classList.add("article-details");
 
   const newsSite = document.createElement("p");
   newsSite.textContent = article.newsSite;
 
   const publicationDate = document.createElement("p");
-  publicationDate.textContent = article.publishedAt;
+  publicationDate.textContent = article.publishedAt.substring(0, 10);
 
   const articleTitle = document.createElement("h3");
   articleTitle.classList.add("article-title");
@@ -38,6 +54,7 @@ const createArticleCard = (article) => {
   const articleLink = document.createElement("a");
   articleLink.classList.add("article-link");
   articleLink.setAttribute("href", article.url);
+  articleLink.textContent = "Read article";
 
   articleDetails.append(newsSite, publicationDate);
   articleCard.append(
@@ -47,4 +64,8 @@ const createArticleCard = (article) => {
     articleSummary,
     articleLink
   );
+
+  return articleCard;
 };
+
+document.addEventListener("DOMContentLoaded", main);
